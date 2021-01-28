7e66d9c64a17cb8bbd54b3a43fc0f60f518ec57d
pedromoraisf <pedro.morais1997@gmail.com>
2021-01-28 10:28:59 -0300

    refactor: removed LogPayload interface into controller protocol

--- src/presentation/controllers/protocols/controller.ts
+++ src/presentation/controllers/protocols/controller.ts
@@ -1,16 +1,11 @@
-export interface LogPayload {
-  title: string
-  body: string
-}
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type PayloadReceive = any
+export type PayloadReceive = any;
 
 export interface PayloadResponse {
-  resultCode: number
-  message: string
+  resultCode: number;
+  message: string;
 }
 
 export interface Controller {
-  handle(payload: PayloadReceive): PayloadResponse
+  handle(payload: PayloadReceive): PayloadResponse;
 }
