bcfa77a664e04ab96414377ede277e8a48b0e1ce
pedromoraisf <pedro.morais1997@gmail.com>
2021-01-28 13:03:13 -0300

    test: add TraceFirebaseStrapi use case implementation

---
+++ src/data/usecases/trace-log/trace-firebase-strapi.spec.ts
@@ -0,0 +1,104 @@
+import { TraceFirebaseStrapi } from "./trace-firebase-strapi";
+import { Log, LogReceive } from "../../../domain/usecases/trace-log";
+import { TraceFirebaseStrapiRepository } from "./../protocols/trace-firebase-strapi-repository";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const makeFakeEntryLog = (): any => ({
+  operation: "",
+  isErr: false,
+  payload: {
+    title: "",
+    body: "",
+  },
+});
+
+const makeFakeLog = (): Log => ({
+  id: 1,
+  operation: "any_operation",
+  isErr: false,
+  payload: {
+    title: "any_title",
+    body: "any_body",
+  },
+  createdAt: new Date(),
+});
+
+const makeFakeTraceFirebaseStrapiRepo = (): TraceFirebaseStrapiRepository => {
+  class FakeTraceFirebaseStrapiRepo {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    async saveLog(log: LogReceive): Promise<Log> {
+      return new Promise((resolve) => resolve(makeFakeLog()));
+    }
+  }
+
+  return new FakeTraceFirebaseStrapiRepo();
+};
+
+interface SutTypes {
+  sut: TraceFirebaseStrapi;
+  fakeTraceFirebaseStrapiRepo: TraceFirebaseStrapiRepository;
+}
+
+const makeSut = (): SutTypes => {
+  const fakeTraceFirebaseStrapiRepo = makeFakeTraceFirebaseStrapiRepo();
+  const sut = new TraceFirebaseStrapi(fakeTraceFirebaseStrapiRepo);
+
+  return {
+    sut,
+    fakeTraceFirebaseStrapiRepo,
+  };
+};
+
+describe("TraceFirebaseStrapi", () => {
+  test("should call treatLog with correct values", async () => {
+    const { sut } = makeSut();
+
+    const spyTreatLog = jest.spyOn(sut, "treatLog");
+
+    const fakeEntryLog = makeFakeEntryLog();
+    await sut.trace(fakeEntryLog);
+
+    expect(spyTreatLog).toHaveBeenCalledWith(fakeEntryLog);
+  });
+
+  describe("should be formatted Log received with default values if it does not contain", () => {
+    const makeFakeDefaultLog = () => ({
+      operation: "",
+      isErr: false,
+      payload: {
+        title: "",
+        body: "",
+      },
+    });
+
+    test("case operation", () => {
+      const { sut } = makeSut();
+
+      const testableLog = makeFakeEntryLog();
+      testableLog.operation = 1;
+
+      expect(sut.treatLog(testableLog)).toEqual(makeFakeDefaultLog());
+    });
+
+    test("case payload", () => {
+      const { sut } = makeSut();
+
+      const testableLog = makeFakeEntryLog();
+      testableLog.payload.title = 1;
+      testableLog.payload.body = false;
+
+      expect(sut.treatLog(testableLog)).toEqual(makeFakeDefaultLog());
+    });
+  });
+
+  test("should call saveLog with correct values", async () => {
+    const { sut, fakeTraceFirebaseStrapiRepo } = makeSut();
+
+    const spyRepository = jest.spyOn(fakeTraceFirebaseStrapiRepo, "saveLog");
+
+    const fakeEntryLog = makeFakeEntryLog();
+    await sut.trace(fakeEntryLog);
+
+    expect(spyRepository).toHaveBeenCalledWith(fakeEntryLog);
+  });
+});
