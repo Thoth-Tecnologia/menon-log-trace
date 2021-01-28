be2fac68ea53ed58c7b4c465c68671f476c977f9
pedromoraisf <pedro.morais1997@gmail.com>
2021-01-28 09:59:46 -0300

    refactor: organize controller types

--- src/presentation/controllers/protocols/controller.ts
+++ src/presentation/controllers/protocols/controller.ts
@@ -1,11 +1,10 @@
-import { LogReceive } from '../../../domain/usecases/trace-log/trace-log-protocols'
-
 export interface LogPayload {
   title: string
   body: string
 }
 
-export type PayloadReceive = LogReceive
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type PayloadReceive = any
 
 export interface PayloadResponse {
   resultCode: number
