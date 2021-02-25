3845f74aa46b468c4b6b846969da8b324948a528
pedromoraisf <pedro.morais1997@gmail.com>
2021-02-25 12:48:02 -0300

    feat: expose generic methods in compose

--- src/main/compose.ts
+++ src/main/compose.ts
@@ -1,7 +1,9 @@
 import globalChunk from "./chunks/global";
 import notificationChunk from "./chunks/notification";
+import genericChunk from "./chunks/generic";
 
 export default {
   ...globalChunk,
   ...notificationChunk,
+  ...genericChunk,
 };
