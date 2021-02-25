5ce22aa934b3419d78dfc83b0324489e407d63d8
pedromoraisf <pedro.morais1997@gmail.com>
2021-02-25 12:48:20 -0300

    test: expose generic methods in compose

--- src/main/compose.spec.ts
+++ src/main/compose.spec.ts
@@ -1,9 +1,17 @@
 import menonLogTrace from "./compose";
 
 describe("Main Layer Integration", () => {
-  test("should be contains all released properties", () => {
+  test("should be contains global properties", () => {
     expect(menonLogTrace).toHaveProperty("setBaseUrl");
+  });
+
+  test("should be contains notification log properties", () => {
     expect(menonLogTrace).toHaveProperty("setNotificationTraceLogEndpoint");
     expect(menonLogTrace).toHaveProperty("notificationTraceLog");
-  });
+  })
+
+  test("should be contains generic log properties", () => {
+    expect(menonLogTrace).toHaveProperty("setGenericLogEndpoint");
+    expect(menonLogTrace).toHaveProperty("genericLog");
+  })
 });
