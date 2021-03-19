6618a1237f01256ec2791b32d0a5a4325b87e6f9
pedromoraisf <pedro.morais1997@gmail.com>
2021-03-19 09:32:39 -0300

    feat: new syntax for generic logs prevents race conditions

--- src/main/chunks/generic/index.ts
+++ src/main/chunks/generic/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { PayloadResponse } from "@presentation/controllers/protocols";
 import makeGenericLogController from "../../factories/generic-log";
@@ -5,5 +6,8 @@ import { genericModule } from "@infra/api/helper/modules";
 
 export default {
   setGenericLogEndpoint: (endpoint: string): void => genericModule().setEndpoint(endpoint),
-  genericLog: (payload: any): Promise<PayloadResponse> => makeGenericLogController().handle(payload),
+  genericLog: (payload: any, endpoint?: string): Promise<PayloadResponse> => {
+    if (endpoint && typeof endpoint === "string") genericModule().setEndpoint(endpoint)
+    return makeGenericLogController().handle(payload)
+  },
 };
